d77c125114de1942b9c30700e349e4b2c5218fae
dada85o <davidsticha13@seznam.cz>
2025-11-13 13:38:08 +0100

    Nefunkční základ

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrace</title>
+    
+    <link rel="stylesheet" href="style/style.css">  
+    <script src="script/script.js"defer></script>
+</head>
+
+
+<body>
+    <main>
+    <section>
+        <div class="form">
+                <div class="form-header">
+                    <img src="img/apexlogo.png" alt="">
+                    <h2>Create an account</h2>
+                </div>
+            </form>
+        </div>
+
+        <form action="#">
+            <div class="form-main">
+                <div class="form-main-inputs">
+                    <div class="form-main-inputs-label">
+                        <img src="" alt="">
+                        <label for="name">Password</label>
+                    </div>
+                    <div class="form-button">
+                    </div>
+                </div>
+            </div>
+        </form>
+
+        <form action="#">
+            <div class="form-main">
+                <div class="form-main-inputs">
+                    <div class="form-main-inputs-label">
+                        <img src="" alt="">
+                        <label for="name">Confirm Password</label>
+                    </div>
+                    <div class="form-button">
+                    </div>
+                </div>
+            </div>
+        </form>
+
+
+
+
+    </section>
+    </main>
+</body>
+</html>
